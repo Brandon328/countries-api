5f54912f04ba8b045d4eaefa08e554d634428aa2
Brandon328 <70345941@continental.edu.pe>
2023-03-17 21:16:13 -0500

    agregue el dark-mode-toggle de chrome

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
-// import './style.css'
+import './reset.css'
+import './style.css'
 // import javascriptLogo from './javascript.svg'
 
 import './js/country-card.js';
 import './js/country-tag.js';
 import './js/country-detail.js';
+import './slider.css';
+import 'dark-mode-toggle';
