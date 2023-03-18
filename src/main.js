cdf901080bdf58435f6ebadd076a31a6e2cc6973
Brandon328 <70345941@continental.edu.pe>
2023-03-18 18:29:58 -0500

    estilos del header

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
-import './reset.css'
+// Styles
 import './style.css'
-// import javascriptLogo from './javascript.svg'
 
+// Web components
 import './js/country-card.js';
 import './js/country-tag.js';
 import './js/country-detail.js';
-import './slider.css';
 import 'dark-mode-toggle';
