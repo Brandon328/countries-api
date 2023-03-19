d203326fb32513dc1cce17eb3c553f052980aceb
Brandon328 <70345941@continental.edu.pe>
2023-03-18 19:23:00 -0500

    diseño de los cards de los países

--- src/js/country-card.js
+++ src/js/country-card.js
@@ -15,23 +15,56 @@ class CountryCard extends HTMLElement {
     const template = document.createElement('template');
     template.innerHTML = `
       <img src="${this.flag}" alt="${this.name} flag">
-      <h2>${this.name}</h2>
-      <p>
-        <b>Population: </b><span>${this.population}</span>
-      </p>
-      <p>
-        <b>Region: </b><span>${this.region}</span>
-      </p>
-      <p>
-        <b>Capital: </b><span>${this.capital}</span>
-      </p>
+      <div>
+        <h2>${this.name}</h2>
+        <p>
+          <b>Population: </b><span>${this.population}</span>
+        </p>
+        <p>
+          <b>Region: </b><span>${this.region}</span>
+        </p>
+        <p>
+          <b>Capital: </b><span>${this.capital}</span>
+        </p>
+      </div>
+      ${this.getStyle()}
     `;
     return template;
   }
   getStyle() {
     return `
       <style>
-      
+        :host{
+          display: block;
+          min-width: 300px;
+          max-width: 400px;
+          width: 100%;
+          min-height: 360px;
+          background-color: var(--dark-mode-text-and-light-mode-elements);
+          box-shadow: 1px 3px 6px 0px rgba(115, 115, 115, 0.15);
+          border-radius: 6px;
+          cursor: pointer;
+        }
+        img{
+          border-radius: 6px 6px 0 0;
+          width: 100%;
+          position: relative;
+        }
+        div{
+          padding: 20px;
+        }
+        div h2, div p{
+          margin: 0;
+        }
+        div h2{
+          margin-bottom: 20px;
+        }
+        div p{
+          margin-bottom: 8px;
+        }
+        div p b{
+          font-weight: 600;
+        }
       </style>
     `;
   }
